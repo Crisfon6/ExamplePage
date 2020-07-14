3ddbc546fd311489215354094548d345d6864338
Cristhian Fonseca <cdelgado376@unab.edu.co>
2020-07-14 18:30:43 -0500

    add all of before page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+// modules
+import {InicioComponent} from './components/inicio/inicio.component';
+import {NosotrosComponent} from './components/nosotros/nosotros.component';
+import {GaleriaComponent} from './components/galeria/galeria.component';
+import {ContactoComponent} from './components/contacto/contacto.component';
+import {AdminComponent} from './components/admin/admin.component';
+import {CrearComponent} from './components/crear/crear.component';
+import {EditarComponent} from './components/editar/editar.component';
+
+const routes: Routes = [
+  {path: "inicio",component:InicioComponent},
+  {path: "nosotros",component:NosotrosComponent},
+  {path: "galeria",component:GaleriaComponent},
+  {path: "contacto",component:ContactoComponent},
+  {path: "admin",component:AdminComponent},
+  {path: "admin/editar",component:EditarComponent},
+  {path: "admin/crear",component:CrearComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CrearComponent } from './components/crear/crear.component';
+import { EditarComponent } from './components/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    GaleriaComponent,
+    ContactoComponent,
+    CarruselComponent,
+    FooterComponent,
+    LoginComponent,
+    AdminComponent,
+    CrearComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
